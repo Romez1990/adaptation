6ce7988fc8acb004db66064020175adb371643d7
Dener <dener7a23c@gmail.com>
2021-03-25 11:14:34 +0300

    test

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
+    path('auth/', TemplateView.as_view(template_name='auth.html')),
 ]
