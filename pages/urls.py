cc7e5bfe846ff10882edb456e1dcf29e504bab68
Dener <dener7a23c@gmail.com>
2021-03-25 14:22:59 +0300

    text

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
-    path('auth/', TemplateView.as_view(template_name='auth.html')),
+    path('auth/', TemplateView.as_view(template_name='index.html')),
     path('main/', TemplateView.as_view(template_name='main.html')),
+    path('user/', TemplateView.as_view(template_name='user.html')),
 ]
