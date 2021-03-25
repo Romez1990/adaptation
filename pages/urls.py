edcae52d4ad724dc44cf4f15ff4d4394a341a0a7
Dener <dener7a23c@gmail.com>
2021-03-25 12:46:12 +0300

    test

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('auth/', TemplateView.as_view(template_name='auth.html')),
+    path('main/', TemplateView.as_view(template_name='main.html')),
 ]
--- static/js/function.js
+++ static/js/function.js
@@ -1,23 +1,53 @@
-
-function auth(){
-    console.log('true')
-    console.log($(this).serialize());
+if(!localStorage.getItem('token') && window.location.href.indexOf('/auth/') === -1){
+     window.location.href = '/auth/'
+    console.log(window.location.href)
 }
 
+if(localStorage.getItem('userStatus') === '1'){
+
+}
+
+//Вход
 $( "#form-user" ).on( "submit", async function( event ) {
     event.preventDefault();
 
     let dataAuthUser = $('#form-user').serialize();
 
     const result = await request('auth/login/','post',dataAuthUser)
-    console.log(result);
+    console.log(result)
+    localStorage.setItem('token',result.token);
+    window.location.href = '/main/'
+
+    getUser();
 });
 
-function request(url, method, body) {
+getUser()
+
+function getUser(){
+    $.ajax({
+        url: '/../api/auth/profile/',
+        type: 'GET',
+        dataType: 'json',
+        headers: {'Authorization': `Token ${localStorage.getItem('token')}`},
+        success: function(result) {
+            console.table(result)
+        }
+    });
+}
+
+//Выход
+$('#logout').on('click', (e)=>{
+    e.preventDefault();
+    localStorage.removeItem('token');
+    window.location.href = '/auth/'
+})
+
+function request(url, method, body,head) {
     const baseUrl = '/api/';
     return new Promise((resolve, reject) =>
         $.ajax(baseUrl + url, {
             type: method,
+            headers: head,
             data: body,
             success: resolve,
             error: reject,
